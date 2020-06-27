157c1f28973217472856983aea28686be873954f
y00rim <yoorimgye@gmail.com>
2020-06-27 23:33:01 +0200

    Updates

---
+++ precache-manifest.86e3994227de4ebda85a19441188d5c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9ad06b58464c7bb78074903c8618023",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "9a51f0ef08d2482d9464",
+    "url": "/movie_app_2020/static/css/main.5a4bb63c.chunk.css"
+  },
+  {
+    "revision": "0a9aca7e10b2aa811635",
+    "url": "/movie_app_2020/static/js/2.2a5f8838.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/movie_app_2020/static/js/2.2a5f8838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a51f0ef08d2482d9464",
+    "url": "/movie_app_2020/static/js/main.43454f02.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
